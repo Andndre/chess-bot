6f9f8a88e7b451a25f11f62e1e2e341c56743930
Andndre <agungandre687@gmail.com>
2022-07-16 19:39:35 +0800

    update playwith command

--- src/commands/prefix/PlayWith.prefix.command.ts
+++ src/commands/prefix/PlayWith.prefix.command.ts
@@ -1,16 +1,27 @@
 import { Message, MessageActionRow, MessageButton } from 'discord.js';
-import { lastElement } from '../../Utils';
+import { getUID, lastElement } from '../../Utils';
 
 export const PlayWith_Prefix = async (message: Message) => {
 	const playerOne = message.author.toString();
 	const playerTwo = lastElement(message.content.split(' '));
 	const row = new MessageActionRow();
+	const gameLinkInfo = await message.reply(
+		'Sending game link for each players...'
+	);
+	const p1UID = getUID(playerOne);
+	const p2UID = getUID(playerTwo);
+	const gameLink =
+		'https://andndre.github.io/chess/play?gameId=' + p1UID + p2UID + '&as=';
+	await message.author.send('Go to ' + gameLink + p1UID);
+	await message.client.users.cache
+		.get(p2UID)!
+		.send('Go to ' + gameLink + p2UID);
 	const button = new MessageButton()
-		.setLabel('Go to the game')
-		.setURL('https://andndre.github.io/chess')
+		.setLabel('Go watch!')
+		.setURL('https://andndre.github.io/chess/watch?gameId=' + p1UID + p2UID)
 		.setStyle('LINK');
 	row.addComponents(button);
-	await message.reply({
+	await gameLinkInfo.edit({
 		content: `${playerOne} vs ${playerTwo} !`,
 		components: [row],
 	});
