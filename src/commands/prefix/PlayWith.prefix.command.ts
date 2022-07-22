3f3ed07c5695c07934e248f3bb9c6599a07288b8
Andndre <agungandre687@gmail.com>
2022-07-22 22:23:33 +0800

    `playwith` command now works.

--- src/commands/prefix/PlayWith.prefix.command.ts
+++ src/commands/prefix/PlayWith.prefix.command.ts
@@ -1,5 +1,6 @@
 import { Message, MessageActionRow, MessageButton } from 'discord.js';
 import { getUID, lastElement } from '../../Utils';
+import fetch from 'cross-fetch';
 
 export const PlayWith_Prefix = async (message: Message) => {
 	const playerOne = message.author.toString();
@@ -8,17 +9,22 @@ export const PlayWith_Prefix = async (message: Message) => {
 	const gameLinkInfo = await message.reply(
 		'Sending game link for each players...'
 	);
-	const p1UID = getUID(playerOne);
+	const res = await fetch('https://chess-web-production.up.railway.app/create');
+	const json = await res.json();
+	const gameId = json.gameId;
+	const whiteKey = json.whiteId;
+	const blackKey = json.blackId;
+	const watchKey = json.watchKey;
 	const p2UID = getUID(playerTwo);
 	const gameLink =
-		'https://andndre.github.io/chess/play?gameId=' + p1UID + p2UID + '&as=';
-	await message.author.send('Go to ' + gameLink + p1UID);
+		'https://chess-web-ten.vercel.app/online?gameId=' + gameId + '&roleKey=';
+	await message.author.send('Go to ' + gameLink + whiteKey);
 	await message.client.users.cache
 		.get(p2UID)!
-		.send('Go to ' + gameLink + p2UID);
+		.send('Go to ' + gameLink + blackKey);
 	const button = new MessageButton()
 		.setLabel('Go watch!')
-		.setURL('https://andndre.github.io/chess/watch?gameId=' + p1UID + p2UID)
+		.setURL(gameLink + watchKey)
 		.setStyle('LINK');
 	row.addComponents(button);
 	await gameLinkInfo.edit({
